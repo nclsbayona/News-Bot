187a9efa00bfcf83f7684554992259919e36210f
nclsbayona <59931437+nclsbayona@users.noreply.github.com>
2021-07-11 04:30:57 +0000

    Initial commit

---
+++ getNews.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+def getLatestNews(country_code: str, api_key: str) -> list:
+    the_news = requests.get(
+        "https://newsapi.org/v2/top-headlines?country={}&apiKey={}".format(
+            country_code, api_key
+        )
+    ).json()
+    return list(the_news.get("articles"))
+
+
+def printArticle(article: dict) -> str:
+    ret="\n"
+    ret+="From:"+ str(article.get("source").get("name"))+"\n"
+    ret+="Author:"+ str(article.get("author"))+"\n"
+    ret+="Title:"+ str(article.get("title"))+"\n"
+    ret+="Description:"+ str(article.get("description"))+"\n"
+    ret+="Url:"+ str(article.get("url"))+"\n"
+    ret+="Image Url:"+ str(article.get("urlToImage"))+"\n"
+    ret+="Publish date:"+ str(article.get("publishedAt"))+"\n"
+    return ret
+
+
+news = getLatestNews("co", os.environ["NEWS_API_KEY"])
+
+if __name__=="__main__":
+  for art in news:
+    print(printArticle(art))
---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import discord
+from discord.ext import tasks
+from getNews import getLatestNews, printArticle
+
+
+api_key = os.environ['NEWS_API_KEY']
+
+channel_id = os.environ['CHANNEL_ID']
+
+bot_token = os.environ['BOT_TOKEN']
+
+class News_Bot_Client(discord.Client):
+  def __init__(self, api_key, channel_id:int, country_code="us", *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.channel_id=channel_id
+    self.channel=None
+    self.country_code=country_code
+    self.api_key=api_key
+    self.latest_news=list()
+    self.getLatest.start()
+  
+  async def on_ready(self):
+    print(f'Logged in as {self.user} (ID: {self.user.id})')
+    print('------')
+
+  @tasks.loop(hours=1) 
+  async def getLatest(self):
+    try:
+      if (self.channel is None):
+        self.channel=self.get_channel(self.channel_id)
+      if (self.country_code==""):
+        raise Exception()
+      the_news=getLatestNews(self.country_code, self.api_key)
+      if (len(self.latest_news)==0):
+        self.latest_news=the_news
+        for (msg) in (the_news):
+          await (self.channel).send(printArticle(msg))
+      elif (the_news!=self.latest_news):
+        new_news=list()
+        for (art) in (the_news):
+          if (art not in self.latest_news):
+            new_news.insert(0, art)
+            self.latest_news.insert(0, art)
+            if (len(self.latest_news)>1):
+              self.latest_news.pop()
+        for (msg) in (new_news):
+          await (self.channel).send(printArticle(msg))
+  
+    except:
+      await (self.channel).send("Please update country code")
+      
+  @getLatest.before_loop
+  async def before_getLatest(self):
+    await self.wait_until_ready()
+
+client = News_Bot_Client(api_key, int(channel_id), "co")
+client.run(bot_token)
